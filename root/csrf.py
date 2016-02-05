175af3485e60373efd8f0bbb40526cc98910b30c
unknown <Alex Richman@CILIX-PC08.cilix.local>
2016-02-05 10:56:22 +0000

    change csrf template function to context processor

--- root/csrf.py
+++ root/csrf.py
@@ -4,14 +4,16 @@ import os
 
 
 def init(app):
-    @app.template_global("get_csrf_token")
-    def get_csrf_token():
+    @app.context_processor
+    def _csrf_token_():
         if("_CSRF_TOKEN_" not in flask.session):
             Hash = hashlib.sha256()
             Hash.update(os.urandom(128))
             flask.session["_CSRF_TOKEN_"] = Hash.hexdigest()
 
-        return flask.session["_CSRF_TOKEN_"]
+        return {
+            "_CSRF_TOKEN_": flask.session["_CSRF_TOKEN_"],
+        }
 
 
     @app.before_request
