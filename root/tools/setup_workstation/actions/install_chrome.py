1b1b03f0b925e78f87d4f82cc90a301baf1e5b58
Alex Richman <alex@richman.io>
2016-02-12 16:40:32 +0000

    remove redundant main function

--- root/tools/setup_workstation/actions/install_chrome.py
+++ root/tools/setup_workstation/actions/install_chrome.py
@@ -3,17 +3,12 @@ import subprocess
 import os
 
 
-def main():
-    print("    Downloading installer...")
-    with open("C:\\Cilix\\Chrome-Installer.msi", "wb") as fp:
-        fp.write(requests.get("https://dl.google.com/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi").content)
+print("    Downloading installer...")
+with open("C:\\Cilix\\Chrome-Installer.msi", "wb") as fp:
+    fp.write(requests.get("https://dl.google.com/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi").content)
 
-    print("    Executing installer...")
-    subprocess.call(["msiexec.exe", "/i", "C:\\Cilix\\Chrome-Installer.msi")
+print("    Executing installer...")
+subprocess.call(["msiexec.exe", "/i", "C:\\Cilix\\Chrome-Installer.msi")
 
-    print("    Cleaning up...")
-    os.remove("C:\\Cilix\\Chrome-Installer.msi")
-
-
-if(__name__ == "__main__"):
-    main()
+print("    Cleaning up...")
+os.remove("C:\\Cilix\\Chrome-Installer.msi")
