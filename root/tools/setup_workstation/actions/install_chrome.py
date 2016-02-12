4137c093574bdf42b80736aaf90dff76dae1fae0
Alex Richman <alex@richman.io>
2016-02-12 16:25:15 +0000

    fix temporary file paths

--- root/tools/setup_workstation/actions/install_chrome.py
+++ root/tools/setup_workstation/actions/install_chrome.py
@@ -5,14 +5,14 @@ import os
 
 def main():
     print("    Downloading installer...")
-    with open("C:\\Cilix\\PCSetup\\Chrome-Installer.msi", "wb") as fp:
+    with open("C:\\Cilix\\Chrome-Installer.msi", "wb") as fp:
         fp.write(requests.get("https://dl.google.com/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi").content)
 
     print("    Executing installer...")
-    subprocess.call(["msiexec.exe", "/i", "C:\\Cilix\\PCSetup\\Chrome-Installer.msi")
+    subprocess.call(["msiexec.exe", "/i", "C:\\Cilix\\Chrome-Installer.msi")
 
     print("    Cleaning up...")
-    os.remove("C:\\Cilix\\PCSetup\\Chrome-Installer.msi")
+    os.remove("C:\\Cilix\\Chrome-Installer.msi")
 
 
 if(__name__ == "__main__"):
