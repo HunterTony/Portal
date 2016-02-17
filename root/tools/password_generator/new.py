1afda0a30a67aa9ada2fc47a7a152e9bf7d475b7
unknown <Alex Richman@CILIX-PC08.cilix.local>
2016-02-17 10:21:03 +0000

    fix dictionary sorting

--- root/tools/password_generator/new.py
+++ root/tools/password_generator/new.py
@@ -4,4 +4,4 @@ from libs import data
 
 
 def do():
-    return flask.render_template("/tools/password_generator/new.html", available_dictionaries=data.dictionary.available)
+    return flask.render_template("/tools/password_generator/new.html", available_dictionaries=sorted(data.dictionary.available))
